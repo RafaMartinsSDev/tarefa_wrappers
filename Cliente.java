f0655868b663d9715af073846d7f7882d57a98bf
RafaMartinsSDev <155027633+RafaMartinsSDev@users.noreply.github.com>
2024-12-19 09:12:06 -0400

    Add files via upload

---
+++ Cliente.java
@@ -0,0 +1,12 @@
+package br.com.rafael;
+
+public class Cliente {
+
+    public static void main (String args[]) {
+    int num1 = 30;
+    short num2 = (short) num1;
+
+    int numI = (int) num2;
+    System.out.println(numI);
+    }
+}
